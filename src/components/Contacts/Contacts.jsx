1cddfa9bb8c9dab7be8d3ea1f312d587eb4bb0f8
MysOlha <misarosh70@gmail.com>
2022-10-22 19:41:03 +0300

    localStorage

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,17 +1,28 @@
-import css from './Contacts.module.css'
+import css from './Contacts.module.css';
+import { IoIosContact } from 'react-icons/io';
 
-const Contacts = ({contacts, onRemove}) => {
-return (
+const Contacts = ({ contacts, onRemove }) => {
+  return (
     <ul className={css.contactList}>
-        {contacts.map(({id, name, number})=>(
-            <li key={name} className={css.contactListItem}>
-                {name}: {number} <button type="button" onClick={() => onRemove(id)} className={css.btnContactList}>Delete</button>
-            </li>
-        ))}
+      {contacts.map(({ id, name, number }) => (
+        <li key={name} className={css.contactListItem}>
+          <IoIosContact
+            size="20px"
+            color="rgb(85, 75, 75)"
+            style={{ marginRight: '5px' }}
+          />
+          {name}: {number}{' '}
+          <button
+            type="button"
+            onClick={() => onRemove(id)}
+            className={css.btnContactList}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
     </ul>
-)
+  );
+};
 
-}
-
-export default Contacts
-
+export default Contacts;
